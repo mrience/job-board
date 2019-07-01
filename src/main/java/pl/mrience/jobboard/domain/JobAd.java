08f039bef32b0f94294902bb72ca4459c171d78a
mateusz <kucka.mateusz14@gmail.com>
2019-07-01 15:35:05 +0200

    problem solved

--- src/main/java/pl/mrience/jobboard/domain/JobAd.java
+++ src/main/java/pl/mrience/jobboard/domain/JobAd.java
@@ -4,6 +4,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.data.annotation.CreatedDate;
+import pl.mrience.jobboard.domain.Company;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotBlank;

b2d0d3fef9e0b08f1ba4782907965019e392a3e6
mateusz <kucka.mateusz14@gmail.com>
2019-07-27 23:44:46 +0200

    custom queries for nip of company

--- src/main/java/pl/mrience/jobboard/repository/CompanyRepository.java
+++ src/main/java/pl/mrience/jobboard/repository/CompanyRepository.java
@@ -1,10 +1,22 @@
 package pl.mrience.jobboard.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import pl.mrience.jobboard.domain.Company;
 import pl.mrience.jobboard.domain.JobAd;
 
+import javax.transaction.Transactional;
+
 @Repository
 public interface CompanyRepository extends JpaRepository <Company, Integer> {
+
+    @Query("SELECT CASE WHEN count(c) > 0 THEN true ELSE false END FROM Company c WHERE nip = c.nip")
+    public boolean existsByNip (String nip);
+
+    @Transactional
+    @Modifying
+    @Query("DELETE FROM Company c WHERE nip = c.nip")
+    public void deleteByNip (String nip);
 }
